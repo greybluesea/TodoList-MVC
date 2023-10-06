f39a9361aadc429366168727d954443356552ae6
Gan (Tony) YUAN <greybluesea@gmail.com>
2023-10-06 15:54:28 +1300

    modified test file

--- test/isEmail_test.go
+++ test/isEmail_test.go
@@ -9,22 +9,14 @@ func TestIsEmail(t *testing.T) {
 
 	testSlice := []string{"hello", "Tony@gmail.com"}
 
-	if isEmail("Hello World") == false {
-		t.Errorf(" %v is not a valid email address\n", "Hello World")
-	} else {
-		fmt.Printf("%s is an email address\n", "Hello World")
-	}
+	for _, str := range testSlice {
 
-	if isEmail(testSlice[0]) == false {
-		t.Errorf(" %v is not a valid email address\n", testSlice[0])
-	} else {
-		fmt.Printf("%s is an email address\n", testSlice[0])
-	}
+		if isEmail(str) == false {
+			t.Errorf(" %s is not a valid email address\n", str)
+		} else {
+			fmt.Printf("%s is an email address\n", str)
+		}
 
-	if isEmail(testSlice[1]) == false {
-		t.Errorf(" %v is not a valid email address\n", testSlice[1])
-	} else {
-		fmt.Printf("%s is an email address\n", testSlice[1])
 	}
 
 }
